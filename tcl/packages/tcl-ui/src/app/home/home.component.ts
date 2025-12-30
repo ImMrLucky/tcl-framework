8e8e9994a873532005cefb5dfd84b8e1dd8487df
kent hamilton <uidevkent@gmail.com>
2025-12-30 00:37:53 -0500

    fixing login issue

--- tcl/packages/tcl-ui/src/app/home/home.component.ts
+++ tcl/packages/tcl-ui/src/app/home/home.component.ts
@@ -2,8 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
 import { LogoComponent } from '../shared/logo.component';
-import { AuthService } from '../auth.service';
+import { AuthService, User } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +15,9 @@ import { AuthService } from '../auth.service';
     CommonModule,
     RouterModule,
     MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDividerModule,
     LogoComponent
   ],
   templateUrl: './home.component.html',
@@ -19,12 +25,18 @@ import { AuthService } from '../auth.service';
 })
 export class HomeComponent implements OnInit {
   isAuthenticated = false;
+  currentUser: User | null = null;
 
   constructor(private authService: AuthService) {}
 
   ngOnInit() {
     this.authService.currentUser$.subscribe(user => {
+      this.currentUser = user;
       this.isAuthenticated = user !== null;
     });
+  }
+
+  signOut() {
+    this.authService.signOut();
   }
 }
